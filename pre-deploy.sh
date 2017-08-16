846f296ebb07b0b58370d0a845a3cb423794ab41
Jacob Fischer <jacob.t.fischer@gmail.com>
2017-08-16 17:01:35 -0500

    Fix download link being broken and deploy gitignore
    - download link will now work in subdirectories
    - fix gitignore being blank upon deployment, so TravisCI doesn't have to deploy so many useless files

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # remove the lines after this line to the gitignore so they are deployed by TravisCI
-sed -n '/## REMOVE THESE TO DEPLOY/q;p' .gitignore > .gitignore
+sed -n '/## REMOVE THESE TO DEPLOY/q;p' .gitignore > .gitignore.temp
+rm .gitignore
+mv .gitignore.temp .gitignore
