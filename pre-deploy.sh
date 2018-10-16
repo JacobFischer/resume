e549815870387e698cc60eb6e2e004fa79cc41ae
Jacob Fischer <jacob.t.fischer@gmail.com>
2018-10-16 18:09:52 -0500

    Delete/move files for markdown revamp

--- pre-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# remove the lines after this line to the gitignore so they are deployed by TravisCI
-sed -n '/## REMOVE THESE TO DEPLOY/q;p' .gitignore > .gitignore.temp
-rm .gitignore
-mv .gitignore.temp .gitignore
--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-npm install node-sass
-
-cd to-pdf
-
-tar xf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
